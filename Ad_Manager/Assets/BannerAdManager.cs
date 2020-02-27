843562e8927e3a88fbacfc87ecc21ec6a2728663
David Herlihy <32128694+DHerlihy92@users.noreply.github.com>
2020-02-27 10:52:13 +0000

    First Commit
    Created project, added rewarded ad and banner ad

---
+++ Ad_Manager/Assets/BannerAdManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAdManager : MonoBehaviour
+{
+
+    public string gameId = "3486322";
+    public string placementId = "bannerPlacement";
+    public bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(placementId);
+    }
+
+    public void ShowBanner()
+    {
+        Advertisement.Banner.Show(placementId);
+    }
+}
